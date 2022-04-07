b1120bbf657ef22bd0b839a7ef582f02f57bbac9
pyuri0726 <103172997+pyuri0726@users.noreply.github.com>
2022-04-07 21:26:19 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = $(".typed");
+
+  $(function() {
+    typed.typed({
+      strings: ["박유리.", "성실한.", "꾸준한.", "최선을 다하는.", "퍼블리셔."],
+      typeSpeed: 150,
+      loop: true,
+    });
+  });
